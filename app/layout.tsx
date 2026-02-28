7621087e6110966064817cf9c23a8ed6dba37c8e
allenkiakshay <allenkiakshay8322@gmail.com>
2026-02-28 22:16:22 +0530

    fix(analytics): update Google Analytics tracking ID

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <GoogleAnalytics gaId="G-0YLN09XG88" />
+      <GoogleAnalytics gaId="G-KEEZ203GH9" />
       <body
         className={`${nunito.variable} ${fredoka.variable} ${geistMono.variable} antialiased`}
       >
